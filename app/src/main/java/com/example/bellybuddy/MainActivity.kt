482d3e5baaaf0630faff835603af92590cb2f2d1
Tien Dung Le <95424716+tienthedev@users.noreply.github.com>
2025-09-17 09:03:58 -0500

    Added initial project code with simple login UI page.

---
+++ app/src/main/java/com/example/bellybuddy/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.bellybuddy
+
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            // For now, just display the input (in a real app, you'd validate it)
+            val textView = findViewById<TextView>(R.id.textView)
+            textView.text = "Username: $username\nPassword: $password"
+        }
+    }
+}
